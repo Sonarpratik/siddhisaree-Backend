1b6286a13f2491f44440780e22eb57f66b4611ef
Sonarpratik <sonarpratik02.ps@gmail.com>
2023-09-11 22:13:09 +0530

    order process

--- play.js
+++ play.js
@@ -1,26 +1,69 @@
-{
-    "user_id":34334343,
-    "products":[
-    {
-        id:1
-    },
-    {
-        id:2
-    },
-    {
-        id:13
-    },
-    {
-        id:14
-    },
-    {
-        id:15
-    },
-    {
-        id:16
-    },
-    {
-        id:17
-    }
-]
-    }
+merchant_id: 2783223
+order_id: 12s34
+currency: INR
+redirect_url: http://localhost:8000/ccavResponseHandler
+cancel_url: http://localhost:8000/ccavResponseHandler
+language: EN
+billing_country: India
+delivery_country: India
+merchant_param1: single
+merchant_param2: 64d2052b802d5000bbb8005b
+merchant_param3: additional Info.
+merchant_param4: additional Info.
+promo_code: 
+customer_identifier: 
+integration_type: iframe_normal
+merchant_param5: 64da576310f6675f044c3bba
+delivery_name: Prateek Sonar
+billing_name: Prateek Sonar
+billing_email: prateeksonar02@gmail.com
+delivery_address: dffdd
+delivery_tel: 09770707070
+delivery_state: Andhra Pradesh
+delivery_city: city
+delivery_zip: 431109
+billing_address: dffdd
+billing_tel: 09770707070
+billing_state: Chhattisgarh
+billing_city: city
+billing_zip: 431109
+
+
+
+
+
+
+
+
+merchant_id: 2783223
+order_id: 1234
+currency: INR
+redirect_url: http://localhost:8000/ccavResponseHandler
+cancel_url: http://localhost:8000/ccavResponseHandler
+language: EN
+billing_country: India
+delivery_country: India
+merchant_param1: cart
+merchant_param2: additional Info.
+merchant_param3: additional Info.
+merchant_param4: additional Info.
+promo_code: 
+customer_identifier: 
+integration_type: iframe_normal
+merchant_param5: 64da576310f6675f044c3bba
+delivery_name: Prateek Sonar
+billing_name: Prateek Sonar
+billing_email: prateeksonar02@gmail.com
+delivery_address: dffdd
+delivery_tel: 09770707070
+delivery_state: Arunachal Pradesh
+delivery_city: city
+delivery_zip: 431109
+billing_address: Santacruz
+billing_tel: 2783223
+billing_state: Chhattisgarh
+billing_city: city
+billing_zip: 400054
+product_ids[0]: 64d1d517485c39a0cde6f6a4
+product_ids[1]: 64d2052b802d5000bbb8005b
+amount: 8100
