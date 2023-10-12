ec65b42d1affb46cb377bb6fd5cc312eea09692e
Sonarpratik <sonarpratik02.ps@gmail.com>
2023-10-12 22:30:36 +0530

    quantity update

--- play.js
+++ play.js
@@ -1,1 +1,40 @@
-MONGO_URL='mongodb+srv://siddhisarees:BZMuyPwjWWko0og3@cluster0.x3dnzg2.mongodb.net/?retryWrites=true&w=majority' SECRET_KEY='toBEmzbV7VL0A3QB' CLIENT_ID='287107085331-o3tpuma26c2et6chg7vep6h259erkv22.apps.googleusercontent.com' SECRET_AUTH_KEY='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'
+
+const oldData=
+{
+    _id: new ObjectId("64e4cbd9baa74dda12b2f17a"),
+    user_id: '64d20a439748af3be87eb313',
+    products: [
+      {
+        quantity: 1,
+        product_id: '64d1d514485c39a0cde6f6a0',
+       
+        _id: new ObjectId("64e4f1e33f8ec8704273a513")
+      },
+      {
+        quantity: 1,
+        product_id: '64d1d517485c39a0cde6f6a4',
+       
+        _id: new ObjectId("64e4f1e53f8ec8704273a582")
+      },
+      {
+        quantity: 1,
+        product_id: '64d1d514485c39a0cde6f6a0',
+       
+        _id: new ObjectId("64e4f3533f8ec8704273a863")
+      }
+    ],
+  }
+there is a products.quantity in 1st oldData.now give products.quantity in newData whos products._id is equals to products.product_id in oldData
+
+
+  const newData={
+    "_id": "64e4cbd9baa74dda12b2f17a",
+    "user_id": "64d20a439748af3be87eb313",
+    "products": [
+        {
+          
+            "_id": "64d1d517485c39a0cde6f6a4",
+   
+        }
+    ]
+}
